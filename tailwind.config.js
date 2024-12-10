d9468ffdb1644a539538711906f6f57502cad889
Parsa12458 <parsa.shirafkan127@gmail.com>
2024-12-10 09:34:01 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      background: "#DDE7DD",
+      brown: "#6D4C3D",
+      paleGreen: "#CDDCCD",
+      red: "#B82828",
+    },
+  },
+  plugins: [],
+};
