d8ec0eb9c23d93c5db32b842e4dfc87812398482
Parsa12458 <parsa.shirafkan127@gmail.com>
2025-01-03 18:45:10 +0330

    use compound component method for the modal functionality

--- src/pages/PetsPage.jsx
+++ src/pages/PetsPage.jsx
@@ -7,17 +7,17 @@ function PetsPage() {
   return (
     <div>
       <div>
-        <Button
-          additionalStyles="flex items-center justify-center gap-2 py-2 px-6"
-          onClick={() => document.getElementById("addPet").showModal()}
-        >
-          <img src="/icons/add-icon.svg" className="w-5" />
-          <span>افزودن پت</span>
-        </Button>
-        <Modal
-          content={<PetForm title="مشخصات پت خود را وارد کنید." />}
-          id="addPet"
-        />
+        <Modal>
+          <Modal.Open opens="addPet">
+            <Button additionalStyles="flex items-center justify-center gap-2 py-2 px-6">
+              <img src="/icons/add-icon.svg" className="w-5" />
+              <span>افزودن پت</span>
+            </Button>
+          </Modal.Open>
+          <Modal.Window name="addPet">
+            <PetForm title="مشخصات پت خود را وارد کنید." />
+          </Modal.Window>
+        </Modal>
       </div>
       <PetsList />
     </div>
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,19 +1,97 @@
-function Modal({ content, id }) {
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useOutsideClick } from "../hooks/useOutsideClick";
+import { createPortal } from "react-dom";
+
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+  const [show, setShow] = useState(false);
+  const [transitionClass, setTransitionClass] = useState("");
+
+  const close = () => setOpenName("");
+  const open = (name) => setOpenName(name);
+
+  useEffect(() => {
+    if (openName) {
+      document.body.style.overflow = "hidden";
+      setShow(true);
+      setTimeout(() => {
+        setTransitionClass(
+          "opacity-100 transition-opacity duration-200 ease-[cubic-bezier(0,0,0.2,1)]",
+        );
+      }, 10);
+    } else {
+      setTransitionClass(
+        "opacity-0 transition-opacity duration-200 ease-[cubic-bezier(0,0,0.2,1)]",
+      );
+    }
+  }, [openName]);
+
+  const handleTransitionEnd = () => {
+    if (!openName) {
+      setShow(false);
+    }
+  };
+
   return (
-    <dialog id={id} className="modal">
-      <div className="modal-box max-w-3xl bg-background px-16 py-12">
-        {content}
-        <form method="dialog">
-          <button className="btn btn-circle btn-ghost btn-sm absolute right-3 top-3">
-            ✕
-          </button>
-        </form>
-      </div>
-      <form method="dialog" className="modal-backdrop">
-        <button>close</button>
-      </form>
-    </dialog>
+    <ModalContext.Provider
+      value={{
+        openName,
+        close,
+        open,
+        show,
+        transitionClass,
+        handleTransitionEnd,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
   );
 }
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close, show, transitionClass, handleTransitionEnd } =
+    useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
+  if (name !== openName) return null;
+
+  return createPortal(
+    <div
+      className={`fixed inset-0 z-50 h-screen w-full bg-black/50 backdrop-blur-[2px] ${show ? "block" : "hidden"} ${transitionClass}`}
+      onTransitionEnd={handleTransitionEnd}
+    >
+      <div
+        ref={ref}
+        className="fixed left-1/2 top-1/2 max-h-[90%] -translate-x-1/2 -translate-y-1/2 overflow-y-auto rounded-3xl bg-background px-16 py-12"
+      >
+        <button
+          onClick={close}
+          className="btn btn-circle btn-ghost btn-xs absolute right-4 top-4"
+        >
+          ✕
+        </button>
+        <div>{children}</div>
+      </div>
+    </div>,
+    document.body,
+  );
+}
+
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
